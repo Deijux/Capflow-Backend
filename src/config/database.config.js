3e48ed6374205ca931a6ce0eaa7d85185abac916
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-29 13:11:38 -0500

    Code refactoring

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-	try {
-		const uri = process.env.MONGO_URI
-		if (!uri) {
-			throw new Error('MONGO_URI no está definida en las variables de entorno')
-		}
-		await mongoose.connect(uri, {})
-		console.log('Base de datos conectada')
-	} catch (error) {
-		console.error('Error al conectar la base de datos:', error)
-		process.exit(1)
-	}
+  try {
+    const uri = process.env.MONGO_URI
+    if (!uri) {
+      throw new Error('MONGO_URI no está definida en las variables de entorno')
+    }
+    await mongoose.connect(uri, {})
+    console.log('Base de datos conectada')
+  } catch (error) {
+    console.error('Error al conectar la base de datos:', error)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const detailSchema = new Schema({
-	size: { type: String, required: true },
-	stock: { type: Number, required: true, min: 0 },
+  size: { type: String, required: true },
+  stock: { type: Number, required: true, min: 0 },
 })
 
 const productSchema = new Schema({
-	name: { type: String, required: true, trim: true },
-	description: { type: String, required: true, trim: true },
-	price: { type: Number, required: true, min: 0 },
-	details: { type: [detailSchema], required: true },
+  name: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  price: { type: Number, required: true, min: 0 },
+  details: { type: [detailSchema], required: true },
 })
 
 const Product = model('Product', productSchema)
