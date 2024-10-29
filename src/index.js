df42771f2221c33dac2eed11d306dea8c46668a1
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-28 21:49:40 -0500

    Import config database and products routes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-const dotenv = require("dotenv");
-const express = require("express");
+const dotenv = require('dotenv')
+const express = require('express')
+const connectDB = require('./config/database.config')
+const productsRoutes = require('./routes/product.routes')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+connectDB()
 
-const PORT = process.env.PORT ?? 3000;
+const app = express()
 
-app.use(express.json());
+const PORT = process.env.PORT ?? 3000
 
-app.get("/", async (req, res) => {
-  res.send("Capflow Backend");
-});
+app.use(express.json())
+
+app.use('/api/products', productsRoutes)
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
