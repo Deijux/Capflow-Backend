3f47d4b2b3284d6a96fe581a8fa765dc0bced2d2
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-28 21:46:19 -0500

    add product model file

---
+++ src/models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const detailSchema = new Schema({
+	size: { type: String, required: true },
+	stock: { type: Number, required: true, min: 0 },
+})
+
+const productSchema = new Schema({
+	name: { type: String, required: true, trim: true },
+	description: { type: String, required: true, trim: true },
+	price: { type: Number, required: true, min: 0 },
+	details: { type: [detailSchema], required: true },
+})
+
+const Product = model('Product', productSchema)
+
+module.exports = Product
