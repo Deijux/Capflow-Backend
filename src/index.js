f62d111b4ec4dce0bbca6733a5c3367e060a4550
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-29 21:08:44 -0500

    Add routes for admin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv')
 const express = require('express')
 const connectDB = require('./config/database.config')
 const productsRoutes = require('./routes/product.routes')
+const adminRoutes = require('./routes/admin.routes')
 
 dotenv.config()
 
@@ -14,7 +15,8 @@ const PORT = process.env.PORT ?? 3000
 app.use(express.json())
 
 app.use('/api/products', productsRoutes)
+app.use('/auth', adminRoutes)
 
 app.listen(PORT, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
