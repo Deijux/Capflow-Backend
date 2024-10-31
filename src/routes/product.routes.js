4aa29d224b31d417037560610ca4212abaf2211e
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-31 14:22:03 -0500

    Add multer middleware

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('../middlewares/multer.middleware')
 const { validateProduct } = require('../validators/products.validator')
 const { authMiddleware } = require('../middlewares/auth.middleware')
 const {
@@ -12,7 +13,13 @@ const {
 
 router.get('/', authMiddleware, getProducts)
 router.get('/:id', authMiddleware, getProductById)
-router.post('/', authMiddleware, validateProduct, createProduct)
+router.post(
+  '/',
+  authMiddleware,
+  upload.array('images', 5),
+  validateProduct,
+  createProduct,
+)
 router.put('/:id', authMiddleware, validateProduct, updateProduct)
 router.delete('/:id', authMiddleware, deleteProduct)
 
