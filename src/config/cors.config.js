d114639d773094cdc017800890d4ccb7f2208507
Juan Aguiar <jaguiarcruz17@gmail.com>
2025-04-03 13:44:06 -0500

    Add url frontend

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: 'http://localhost:5173', //(https://your-client-app.com)
+  origin: ['http://localhost:5173', 'https://capflow-deijux.vercel.app'],
   optionsSuccessStatus: 200,
   credentials: true,
 }
